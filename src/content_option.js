dd0ba2f33a9326c8844fb0970eefee68b948eef7
gdhanush27 <dhanushg.21aim@kongu.edu>
2024-05-20 08:10:25 +0530

    Portfolio updated

---
+++ src/content_option.js
@@ -0,0 +1,149 @@
+const logotext = "DHANUSH G";
+const meta = {
+    title: "Dhanush G",
+    description: "I’m Dhanush G, a web developer and AI enthusiast, currently studying in Perundurai, Erode.",
+};
+
+const introdata = {
+    title: "I’m Dhanush G",
+    animated: {
+        first: "I love coding",
+        second: "I develop machine learning models",
+        third: "I create web applications",
+    },
+    description: "As a motivated and dedicated student, I am excited to embark on my journey as a web developer. With a strong foundation in web development technologies gained through coursework and self-study, I am eager to apply my skills to real-world projects.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "A bit about myself",
+    aboutme: "I am pursuing BTech in Artificial Intelligence and Machine Learning at Kongu Engineering College, with a passion for developing innovative solutions using web technologies and machine learning.",
+};
+
+const worktimeline = [{
+        jobtitle: "Representative",
+        where: "Kongu Engineering College",
+        date: "2021-2023",
+    },
+    {
+        jobtitle: "Secretary",
+        where: "AI Coding Club",
+        date: "2022-2023",
+    },
+    {
+        jobtitle: "Joint Secretary",
+        where: "AI Coding Club",
+        date: "Since 2023",
+    },
+    {
+        jobtitle: "Multi-media",
+        where: "AI Association",
+        date: "Since 2023",
+    }
+];
+
+const skills = [{
+        name: "C",
+        value: 85,
+    },
+    {
+        name: "Java",
+        value: 80,
+    },
+    {
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Flask",
+        value: 70,
+    },
+    {
+        name: "Machine Learning",
+        value: 85,
+    },
+    {
+        name: "Git and GitHub",
+        value: 75,
+    },
+    {
+        name: "Organizing abilities",
+        value: 80,
+    },
+    {
+        name: "Time Management",
+        value: 80,
+    },
+];
+
+const services = [
+    {
+        title: "Web Development",
+        description: "Creating responsive and interactive websites using modern web technologies.",
+    },
+    {
+        title: "Machine Learning",
+        description: "Developing machine learning models to solve real-world problems.",
+    },
+    {
+        title: "Deep Learning",
+        description: "Building deep learning models for complex data analysis and predictions.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://cdn.vectorstock.com/i/1000v/43/64/machine-learning-icon-artificial-intelligence-vector-35234364.jpg",
+        description: "Cyclone Detection using Machine Learning",
+        link: "#",
+    },
+    {
+        img: "https://www.medibrandox.com/asset/images/service-banner.webp",
+        description: "Pneumonia Predictor Web UI using Deep Learning, React, and Flask",
+        link: "https://github.com/gdhanush27/Pneumonia-prediction-WebUI",
+    },
+    {
+        img: "https://www.webfx.com/wp-content/uploads/2021/10/oneline_store_example_22tsovet.jpg",
+        description: "Shoping Display of a Company",
+        link: "https://arokya-oil-mill.github.io/",
+    },
+    {
+        img: "https://www.transformdestiny.com/img/nlp-communication-model.jpg",
+        description: "Thanglish to English Translator using Neural Networks",
+        link: "https://github.com/gdhanush27/Tanglish-to-English",
+    },
+    {
+        img: "https://rimage.gnst.jp/livejapan.com/public/article/detail/a/00/05/a0005475/img/en/a0005475_parts_65eea9cc27b11.jpg",
+        description: "SAP Calcuator using teseract OCR",
+        link: "https://github.com/gdhanush27/sap-calculator",
+    }
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "dhanushg.21aim@kongu.edu",
+    YOUR_FONE: "+91 94864 21222",
+    description: "Feel free to reach out to me for any web development or machine learning projects.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/gdhanush27",
+    facebook: "https://www.instagram.com/gdhanush270/",
+    linkedin: "https://www.linkedin.com/in/dhanush-gunasekaran/",
+};
+
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
